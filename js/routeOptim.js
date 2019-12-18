60a4d571219512daab70852a2677ab1cbb4cab9d
skanjsi <53118838+skanjsi@users.noreply.github.com>
2019-12-19 00:52:37 +0900

    RouteBackground2

--- js/routeOptim.js
+++ js/routeOptim.js
@@ -13,7 +13,7 @@ mapboxgl.accessToken =
   "pk.eyJ1IjoidXJiYW5pbiIsImEiOiJjazMya3huZmowMnoyM21waWJiZmJhcXhmIn0.ji6EuAtrz23QDiP-gdw-Yw";
 var map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/urbanin/ck4bdwns428oj1cpfgc1w54dd",
+  style: "mapbox://styles/urbanin/ck4bh2r7u02qh1bp32od7f7ni",
   center: [127.051954, 37.544775], // 지도 중심 경위도
   zoom: 13 // 지도 줌 레벨
 });
